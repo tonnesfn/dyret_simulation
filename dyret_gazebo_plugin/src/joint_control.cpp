a17866df33b32689c5e3b76ba66782a79c63014e
Jørgen Nordmoen <jorgehn@student.matnat.uio.no>
2018-04-18 14:49:34 +0200

    Removed unused C++ files

--- dyret_gazebo_plugin/src/joint_control.cpp
+++
@@ -1,8 +0,0 @@
-#include "dyret_gazebo_plugin/joint_control.hpp"
-
-namespace dyret {
-	void JointControl::SetPID(const gazebo::common::PID pid) {
-		this->pid = pid;
-		ctrl->SetPositionPID(name, this->pid);
-	}
-}
